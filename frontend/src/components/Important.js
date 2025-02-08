ae6970cc76ed03b6f1fb792a9668f1b4319e286c
suzarm <suzarm8883@gmail.com>
2025-02-08 18:41:00 +0400

    Created important.css for high-priority elements styling

---
+++ frontend/src/components/Important.js
@@ -0,0 +1,30 @@
+import '../styles/Important.css';  
+
+
+function Important() {
+    return (
+        <div class="section-important">
+            <h1 class="section-important-title">Что для нас важно</h1>
+            <div class="section-important-container">
+                <div class="section-important-first-container">
+                    <img class="section-important-first-icon" src="/images/quality 1.png"/>
+                    <p class="section-important-first-p">Качество</p>
+                    <p class="section-important-first-text" >Наши профессионалы работают на лучшем оборудовании для пошива одежды беспрецедентного качества</p>
+                </div>
+                <div class="section-important-second-container">
+                    <img class="section-important-second-icon" src="/images/mdi-light_cog.png"/>
+                    <p class="section-important-second-p">Скорость</p>
+                    <p class="section-important-second-text" >Благодаря отлаженной системе в Womazing мы можем отшивать до 20-ти единиц продукции в наших собственных цехах</p>
+                </div>
+                <div class="section-important-third-container">
+                    <img class="section-important-third-icon" src="/images/Frame 16.png"/>
+                    <p class="section-important-third-p">Ответственность</p>
+                    <p class="section-important-third-text" >Мы заботимся о людях и планете. Безотходное производство и комфортные условия труда - все это Womazing</p>
+                </div>
+            </div>
+        </div>
+    );
+  }
+  
+  export default Important;
+  
