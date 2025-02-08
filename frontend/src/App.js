429213c340668c56ba70e2e88a12bb761cbd891e
suzarm <suzarm8883@gmail.com>
2025-02-08 18:42:08 +0400

    "Integrated React Router for handling routing in the app

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,24 +1,26 @@
-import logo from './logo.svg';
+import './styles/normalize.css'
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import './App.css';
+
+import Home from './pages/Home';
+import Store from './pages/Store';
+import About from './pages/About';
+import Contact from './pages/Contact';
+import Basket from './pages/Basket';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <div className="App">
+        <Routes>
+          <Route path="/" element={<Home/>}/>
+          <Route path="/store" element={<Store/>}/>
+          <Route path="/about" element={<About/>}/>
+          <Route path="/contact" element={<Contact/>}/>
+          <Route path="/basket" element={<Basket/>}/>
+        </Routes>
+      </div>
+    </Router>
   );
 }
 
