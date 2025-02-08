2ba42143fe5c875ba327c9b421cf12f26eeba287
suzarm <suzarm8883@gmail.com>
2025-02-08 18:41:15 +0400

    Styled team section with team.css for layout and design

---
+++ frontend/src/components/Team.js
@@ -0,0 +1,24 @@
+import '../styles/Team.css';  
+
+
+function Team() {
+    return (
+        <div class="section-team">
+            <h1 class="section-team-title">Команда мечты Womazing</h1>
+            <div class="section-team-container">
+                <i class="fa-sharp fa-solid fa-arrow-left"></i>
+                <div class="section-team-pic"></div>
+                <i class="fa-sharp fa-solid fa-arrow-right"></i>
+                <div class="section-team-content">
+                    <h4 class="section-team-h4">Для каждой</h4>
+                    <p class='section-team-text-1'>Каждая девушка уникальна. Однако, мы схожи в миллионе мелочей.</p>
+                    <p class='section-team-text-2'>Womazing ищет эти мелочи и создает прекрасные вещи, которые выгодно подчеркивают достоинства каждой девушки.</p>
+                    <p class='section-team-text-3'>Подробнее о бренде</p>
+                </div>
+            </div>
+        </div>
+    );
+  }
+  
+  export default Team;
+  
